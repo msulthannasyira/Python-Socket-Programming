aa53498509ad52ed1b3a134d7a9544ad2d999de5
Muhammad Sulthan Nasyira <114332103+msulthannasyira@users.noreply.github.com>
2024-10-31 13:28:41 +0700

    Add files via upload

---
+++ SOCKET06_PRINTTIME.py
@@ -0,0 +1,12 @@
+# Install ntplib on your Python before doing this.
+
+import ntplib
+from time import ctime
+
+def print_time():
+    ntp_client = ntplib.NTPClient()
+    response = ntp_client.request('pool.ntp.org')
+    print(ctime(response.tx_time))
+
+if __name__ == '__main__':
+    print_time()
